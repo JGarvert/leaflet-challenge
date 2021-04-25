69878452e435c32963db918e4500fbb81b26c479
JGarvert <julie.garvert@adidas.com>
2021-04-25 13:08:11 -0700

    updating logic

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,19 +1,61 @@
-/* NOTE FOR STEP 2
-/  You can use the javascript Promise.all function to make two d3.json calls, 
-/  and your then function will not run until all data has been retreived.
-/
-/ ----------------------------------------
-/  Promise.all(
-/    [
-/        d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"),
-/        d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json")
-/    ]
-/  ).then( ([data,platedata]) => {
-/
-/        console.log("earthquakes", data)
-/        console.log("tectonic plates", platedata)
-/
-/    }).catch(e => console.warn(e));
-/
-/ ----------------------------------------*/
+
+
+
+// Creating map object
+const myMap = L.map("map", {
+	center: [37.09, -95.71],
+	zoom: 3.5,
+	layers: [satellite]
+});
+
+// Add tile layer
+var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+	maxZoom: 18,
+	id: "mapbox.satellite",
+	accessToken: API_KEY
+});	
+
+// Load the geojson data
+geojson_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Grab the data with D3
+d3.json(geojson_url, function (data) {
+
+	for (var i = 0; i < data.features.length; i++) {
+
+		// Conditionals for countries points
+		var color = "";
+		if (data.features[i].properties.mag < 3) {
+			color = "blue";
+		}
+		else if (data.features[i].properties.mag > 3) {
+			color = "yellow";
+		}
+		else if (data.features[i].properties.mag > 4) {
+			color = "orange";
+		}
+		else if (data.features[i].properties.mag > 5) {
+			color = "red";
+		}
+
+		// Add circles to map
+		L.circle([
+			data.features[i].geometry.coordinates[1], 
+			data.features[i].geometry.coordinates[0]], {
+				color: color,
+				fillColor: color,
+				fillOpacity: 0.75,
+				radius: data.features[i].properties.mag * 10000
+		})
+
+		// Add Popup detail
+		.bindPopup("<h2>" + data.features[i].properties.title + "</h2> <hr> <h4>Location: " + data.features[i].properties.place + "</h4>")
+		
+		// Add features to map
+		.addTo(myMap);
+
+	}
+
+});
 
